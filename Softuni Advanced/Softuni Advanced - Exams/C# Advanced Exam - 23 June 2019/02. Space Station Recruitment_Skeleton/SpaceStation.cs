8ae2a5dad1f4e2927dd2e7db4c6197d0bd8d9fe7
Krum Arnaudov <krum4okkkkk@abv.bg>
2019-06-28 00:17:36 +0300

    Add files via upload

---
+++ Softuni Advanced/Softuni Advanced - Exams/C# Advanced Exam - 23 June 2019/02. Space Station Recruitment_Skeleton/SpaceStation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+        private List<Astronaut> astronauts;
+        private int count;
+        public SpaceStation(string name,int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Count = count;
+            astronauts = new List<Astronaut>();
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get; set; }
+
+        public void Add(Astronaut astronaut)
+        {
+            if(Count < Capacity)
+            {
+                this.astronauts.Add(astronaut);
+                Count++;
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (astronauts.Any(a => a.Name == name))
+            {
+                var austronautToDelete = astronauts.First(a => a.Name == name);
+                astronauts.Remove(austronautToDelete);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Astronaut GetOldestAstronaut()
+        {
+            Astronaut oldestAustronaut = astronauts.OrderByDescending(a => a.Age).First();
+            return oldestAustronaut;
+        }
+        public Astronaut GetAstronaut(string name)
+        {
+            Astronaut whatAstrunautToReturn = astronauts.FirstOrDefault(a => a.Name == name);
+            return whatAstrunautToReturn;
+        }
+        public string Report()
+        {
+            return $"Astronauts working at Space Station {this.Name}:" +Environment.NewLine + string.Join(Environment.NewLine,astronauts);
+        }
+    }
+}
