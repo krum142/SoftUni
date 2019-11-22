51d10e50066c5cee68101afb1fa28668a402f35f
krum <krum4o55555@gmail.com>
2019-11-23 00:12:33 +0200

    at new exercises

---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/01. ListyIterator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command = Console.ReadLine();
+
+            ListyIterator<string> listyiterator = null;
+
+            while (command != "END")
+            {
+                try
+                {
+                    if (command.Contains("Create"))
+                    {
+                        List<string> items = command.Split().Skip(1).ToList();
+                        listyiterator = new ListyIterator<string>(items);
+
+                    }
+                    if(command == "HasNext")
+                    {
+                        Console.WriteLine(listyiterator.HasNext());
+                    }
+                    if (command == "Move")
+                    {
+                        Console.WriteLine(listyiterator.Move());
+                    }
+                    if (command == "Print")
+                    {
+                        listyiterator.Print();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+                command = Console.ReadLine();
+            }
+
+        
+            
+        }
+    }
+}
