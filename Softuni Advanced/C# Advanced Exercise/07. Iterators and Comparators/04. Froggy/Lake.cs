9f8bedd5970b11c80b41bec1c6b731b6279bb48d
krum <krum4o55555@gmail.com>
2019-12-08 22:01:41 +0200

    at new exercises

---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/04. Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FroggyJump
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> jumps;
+
+        public Lake(List<int> items)
+        {
+            this.jumps = new List<int>();
+            jumps.AddRange(items);
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < jumps.Count; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    yield return jumps[i];
+                }
+            }
+            for (int j = jumps.Count - 1; j >= 0; j--)
+            {
+                if (j % 2 == 1)
+                {
+                    yield return jumps[j];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/04. Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace FroggyJump
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var jumps = new Lake(input);
+
+            Console.WriteLine(string.Join(", ", jumps));
+        }
+    }
+}
