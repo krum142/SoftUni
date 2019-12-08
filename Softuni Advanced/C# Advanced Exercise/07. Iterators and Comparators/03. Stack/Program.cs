330340fdcfd9fe320d4b864d4a11bcc844ba1973
krum <krum4o55555@gmail.com>
2019-12-08 21:52:47 +0200

    at new exercises

---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/03. Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var stack = new CustomStack<string>();
+
+
+            while (command[0] != "END")
+            {
+
+                if (command[0] == "Push")
+                {
+                    stack.Push(command.Skip(1).ToList());
+                }
+                else if (command[0] == "Pop")
+                {
+                    stack.Pop();
+                }
+
+                command = Console.ReadLine()
+                    .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
