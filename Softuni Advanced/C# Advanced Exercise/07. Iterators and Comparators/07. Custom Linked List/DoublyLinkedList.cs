a228563f1aef72d4ea616e5198bccad9ebeeb2d6
krum <krum4o55555@gmail.com>
2019-12-08 22:38:41 +0200

    at new exercises

---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/07. Custom Linked List/DoublyLinkedList.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomLinkedList
+{
+    public class DoublyLinkedList<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        private class LinkNode
+        {
+            public LinkNode(T value)
+            {
+                Value = value;
+            }
+            public T Value { get; }
+            public LinkNode NextNode { get; set; }
+            public LinkNode PreviousNode { get; set; }
+        }
+
+        private LinkNode head;
+
+        private LinkNode tail;
+        public int Count { get; private set; }
+        public void AddFirst(T value)
+        {
+            LinkNode newHead = new LinkNode(value);
+
+            if (Count == 0)
+            {
+                tail = head = newHead;
+            }
+            else
+            {
+                newHead.NextNode = head;
+                head.PreviousNode = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+        public void AddLast(T value)
+        {
+            LinkNode newTail = new LinkNode(value);
+
+            if (Count == 0)
+            {
+                tail = head = newTail;
+            }
+            else
+            {
+                newTail.PreviousNode = tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+
+            Count++;
+        }
+        public T RemoveFirst()
+        {
+            CheckIfEmptyThrowException();
+
+            T firstElement = head.Value;
+
+            head = head.NextNode;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+            else
+            {
+                head.PreviousNode = null;
+            }
+
+            Count--;
+
+            return firstElement;
+        }
+        public T RemoveLast()
+        {
+            CheckIfEmptyThrowException();
+
+            T lastElement = tail.Value;
+            tail = tail.PreviousNode;
+
+            if (tail == null)
+            {
+                head = null;
+            }
+            else
+            {
+                tail.NextNode = null;
+            }
+
+            Count--;
+
+            return lastElement;
+        }
+        public void Print(Action<T> action)
+        {
+            LinkNode currentNode = head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+
+                currentNode = currentNode.NextNode;
+            }
+        }
+        public bool Contains(T value)
+        {
+            LinkNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value.CompareTo(value) == 0)
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.NextNode;
+            }
+
+            return false;
+        }
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+
+            var currentNode = head;
+            int counter = 0;
+
+            while (currentNode != null)
+            {
+                array[counter++] = currentNode.Value;
+
+                currentNode = currentNode.NextNode;
+            }
+
+            return array;
+        }
+        public List<T> ToList()
+        {
+            List<T> list = new List<T>(Count);
+
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                list.Add(currentNode.Value);
+
+                currentNode = currentNode.NextNode;
+            }
+
+            return list;
+        }
+        private void CheckIfEmptyThrowException()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException
+                    ("DoublyLinkedList is empty!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkNode currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/07. Custom Linked List/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var doublyLinkedList = new DoublyLinkedList<string>();
+
+            doublyLinkedList.AddLast("str1");
+            doublyLinkedList.AddLast("str2");
+            doublyLinkedList.AddLast("str3");
+
+            var array = doublyLinkedList.ToList();
+
+            foreach (var item in doublyLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
