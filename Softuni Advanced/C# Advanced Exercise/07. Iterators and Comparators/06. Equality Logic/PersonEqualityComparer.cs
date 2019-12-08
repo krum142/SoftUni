1caa999584a1cfe2353bf5682644c67481e01082
krum <krum4o55555@gmail.com>
2019-12-08 22:42:16 +0200

    at new exercises

---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/06. Equality Logic/PersonEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿namespace EqualityLogic
+{
+    using System.Collections.Generic;
+
+    public class PersonEqualityComparer : IEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y)
+        {
+            return x.CompareTo(y) == 0;
+        }
+
+        public int GetHashCode(Person person)
+        {
+            return $"{person.Name} {person.Age}".GetHashCode();
+        }
+    }
+}
