204ed84cb13dd2fab22dff3cf86a20540643aa33
krum <krum4o55555@gmail.com>
2019-12-08 21:42:33 +0200

    at new exercises

---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/02. Collection/Collectionn.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class Collectionn<T> : IEnumerable<T>
+    {
+        private List<T> items;
+        private int index;
+        public Collectionn(List<T> items)
+        {
+            this.items = items;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+
+            if(HasNext())
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+            //index
+            //currentIndex < items.lengt
+            //return ture// false  
+        }
+        public bool HasNext()
+        {
+            if (this.index < this.items.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public void Print()
+        {
+            if(this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(this.items[this.index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Softuni Advanced/C# Advanced Exercise/07. Iterators and Comparators/02. Collection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command = Console.ReadLine();
+
+            Collectionn<string> listyiterator = null;
+
+            while (command != "END")
+            {
+                try
+                {
+                    if (command.Contains("Create"))
+                    {
+                        List<string> items = command.Split().Skip(1).ToList();
+                        listyiterator = new Collectionn<string>(items);
+
+                    }
+                    if(command == "HasNext")
+                    {
+                        Console.WriteLine(listyiterator.HasNext());
+                    }
+                    if (command == "Move")
+                    {
+                        Console.WriteLine(listyiterator.Move());
+                    }
+                    if (command == "Print")
+                    {
+                        listyiterator.Print();
+                    }
+                    else if(command == "PrintAll")
+                    {
+                        foreach (var item in listyiterator)
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+                command = Console.ReadLine();
+            }
+
+        
+            
+        }
+    }
+}
