f9db7cb7b48019a98a75709090e2ebee7aba7e7f
krum <krum4o55555@gmail.com>
2019-12-13 20:42:24 +0200

    Add files via upload

---
+++ Softuni OOP/03 Encapsulation/04. Pizza Calories/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using PizzaCalories;
+using System;
+
+namespace StartUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
